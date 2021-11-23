fad71329340ceb3913d1a29ebdb73caafc19424c
phillhamer <p.hamer@unsw.edu.au>
2021-11-23 15:18:19 +1100

    update for graphing

--- IVtesting.py
+++ IVtesting.py
@@ -1,7 +1,7 @@
 import pvlib
 
-params = pvlib.pvsystem.singlediode(photocurrent=13.6, saturation_current=6.29e-12,
-                                    resistance_series=0.18, resistance_shunt=500,
+params = pvlib.pvsystem.singlediode(photocurrent=13.6, saturation_current=9.94e-12,
+                                    resistance_series=0.2, resistance_shunt=500,
                                     nNsVth=1.9)
 
 print(params)
