6e79126abe0c60dfd2bfdce57acfe5007d201103
phillhamer <p.hamer@unsw.edu.au>
2021-11-30 15:29:31 +1100

    tidying up figures

--- monte_carlo_npv_plot.py
+++ monte_carlo_npv_plot.py
@@ -16,9 +16,13 @@ npv_sat_iter = npv_sat_iter.rename(columns={'0':Scenario_name_2})
 
 graph_data_npv = npv_mav_iter.join(npv_sat_iter, rsuffix='p')
 print(graph_data_npv)
+
 graph_data_npv2 = graph_data_npv[[Scenario_name, Scenario_name_2]]
 graph_data_npv2.plot.hist(bins = 50, histtype='step')
-plt.savefig('.\\Data\\OutputData\\' + module_name + ' NPV', pad_inches=0.1)
+xticks, x_tick_labels = plt.xticks()
+plt.xticks(ticks=xticks, labels = xticks/1e6)
+plt.xlabel('Net Present Value ($m)', loc='center')
+plt.savefig('.\\Data\\OutputData\\' + module_name + ' NPV', bbox_inches='tight', pad_inches=0.1)
 plt.show()
 
 LCOE_mav_iter = pd.read_csv(os.path.join('Data', 'OutputData', '5B_MAV ' + module_name + ' LCOE.csv'), index_col=1)
@@ -33,7 +37,10 @@ graph_data_LCOE = LCOE_mav_iter.join(LCOE_sat_iter, rsuffix='p')
 print(graph_data_LCOE)
 graph_data_LCOE2 = graph_data_LCOE[[Scenario_name_LCOE, Scenario_name_LCOE2]]
 graph_data_LCOE2.plot.hist(bins = 50, histtype='step')
-plt.savefig('.\\Data\\OutputData\\' + module_name + ' LCOE', pad_inches=0.1)
+xticks, x_tick_labels = plt.xticks()
+plt.xticks(ticks=xticks, labels = xticks*100)
+plt.xlabel('LCOE (c/kWh)', loc='center')
+plt.savefig('.\\Data\\OutputData\\' + module_name + ' LCOE',bbox_inches='tight', pad_inches=0.1)
 plt.show()
 
 
