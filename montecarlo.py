e75025bda94e8133d97504e06691bc610996cafc
Hoshang Mehta <hoshang@hoshangs-mbp.home>
2022-01-06 10:31:34 +1100

    Finished fixing montecarlo.py

--- montecarlo.py
+++ montecarlo.py
@@ -1,48 +1,38 @@
-# pylint: disable=E0401
-''' This is the main script for the yield assessment. The script calls the functions below
-    to calculate the Net Present Value (NPV) of the solar farm
 
+""" This is the main script for the yield assessment. The script calls the functions below to
+calculate the Net Present Value (NPV) of the solar farm
 1) Weather:
 Input: Weather file, year of simulation
 Output: Weather dataframe in the required format for PVlib simulatins
-
 2) Racks & modules:
 Input: Type of module and rack
-Output: Module and rack parameters required for PVlib PVsystem extracted
-        from the module & rack database
-
+Output: Module and rack parameters required for PVlib PVsystem extracted from the
+module & rack database
 3) Sizing
 Input: DC size of the solar farm
-Output: Configuration in terms of number of single-axis tracking (SAT) racks or
-        5B Maverick units (MAV)
-
+Output: Configuration in terms of number of single-axis tracking (SAT) racks or 5B Maverick
+units (MAV)
 4) DC/AC Yield:
 Input: number of racks, number of MAVs, weather data, module name, mounting type
-      (optional & future improvement:temperature rack type, aoi model,
-       back-tracking/optimal tilt angle)
+(optional & future improvement: temperature rack type, aoi model, back-tracking/optimal tilt angle)
 Output: DC/AC yield
-
 5) Revenue and storage behaviour
 Input: DC yield, export limit
 Output: Revenue
-
 6) Cost (get deterministic or monte-carlo cost distribution of the solar farm)
 Input: DC size pf the solar farm
 Output: Cost
-
 7) Net present value (NPV)
 Input: Revenue, Cost
 Output: NPV
-
 Initially, we will write the Main script to call out all these functions to give a NPV
-Later, we will convert this to an optimization process which will give the
-optimum NPV based on:
-    - Configuration
-    - Mounting
-    - Other parameters (more advanced params like back tracking algo,
-      temp/racking type etc.)
+Later, we will convert this to an optimization process which will give the optimum NPV based on:
+- Configuration
+- Mounting
+- Other parameters (more advanced params like back tracking algo, temp/racking type etc.)
 During the optimisation process/simulations, cost function will give deterministic values.
-Once the optimum NPV is found cost function also give the Monte-Carlo distribution.'''
+Once the optimum NPV is found cost function also give the Monte-Carlo distribution.
+"""
 
 # %% Import
 import importlib
@@ -56,82 +46,70 @@ from suncable_cost import calculate_scenarios_iterations, create_iteration_table
      generate_parameters, calculate_variance_contributions
 
 # %%
-######################## Constants ######################
-# Weather
-SIMULATION_YEARS = [2019]
-WEATHER_FILE = 'Solcast_PT60M.csv'
-weather_simulation = weather(SIMULATION_YEARS, WEATHER_FILE)
+# Set overall conditions for this analysis
 
-'''
-Sizing/rack and module numbers
-Call the constants from the database - unneeded if we just pass module class?
-'''
-# DC size in MW
-DC_TOTAL = 11000
-# Number of smaller zones that will make up the solar farm
-NUM_OF_ZONES = 267
-# Zone Area in m2
-ZONE_AREA = 4e5
-RACK_INTERVAL_RATIO = 0.04
-# Enter one of the modules from the SunCable module database
-MODULE_TYPE = 'HJT_2028_M10'
-INSTALL_YEAR = 2027
-YEAR_ANALYSIS = 2024
+# Weather
+simulation_years = [2019]
+weather_file = 'Solcast_PT60M.csv'
+weather_simulation = weather(simulation_years, weather_file)
+
+# Sizing/rack and module numbers
+# Call the constants from the database - unneeded if we just pass module class?
+DCTotal = 11000  # DC size in MW
+num_of_zones = 267  # Number of smaller zones that will make up the solar farm
+zone_area = 4e5   # Zone Area in m2
+rack_interval_ratio = 0.04
+module_type = 'HJT_2028_M10'  # Enter one of the modules from the SunCable module database
+install_year = 2027
+analysis_year_start = 2024
 
 # Yield Assessment Inputs
-# Choose a temperature model either Sandia: 'sapm' or PVSyst: 'pvsyst'
-TEMP_MODEL = 'sapm'
+temp_model = 'sapm'  # choose a temperature model either Sandia: 'sapm' or PVSyst: 'pvsyst'
 
 # Revenue and storage behaviour
-EXPORT_LIM = 3.2e9/NUM_OF_ZONES
-STORAGE_CAPACITY = 4e7
-SCHEDULED_PRICE = 0.00004  # 4c/kWh (conversion from Wh to kWh)
+export_lim = 3.2e9/num_of_zones
+storage_capacity = 4e7
+scheduled_price = 0.00004  # 4c/kWh (conversion from Wh to kWh)
 
 # Financial Parameters
-DISCOUNT_RATE = 0.07
+discount_rate = 0.07
 
 # cost data tables from airtable
-DATA_TABLES = get_airtable()
+data_tables = get_airtable()
 
-#########################################################
+
+################
 # Optimise SATs
 # %% ======================================
 # Rack_module
-# Choose rack_type from 5B_MAV or SAT_1 for maverick or single axis tracking respectively
-RACK_TYPE = 'SAT_1'
+rack_type = 'SAT_1'  # Choose rack_type from 5B_MAV or SAT_1 for maverick or single axis tracking respectively
 
-scenario_tables_optimum_SAT, revenue_SAT, kWh_export_SAT = optimise_layout(weather_simulation,
-                    RACK_TYPE, MODULE_TYPE, INSTALL_YEAR,
-                    DC_TOTAL, NUM_OF_ZONES, ZONE_AREA, RACK_INTERVAL_RATIO, TEMP_MODEL,
-                    EXPORT_LIM, STORAGE_CAPACITY, SCHEDULED_PRICE,
-                    DATA_TABLES, DISCOUNT_RATE)
+scenario_tables_optimum_SAT, revenue_SAT, kWh_export_SAT = optimise_layout(weather_simulation,\
+     rack_type, module_type, install_year, DCTotal, num_of_zones, zone_area, rack_interval_ratio,\
+          temp_model, export_lim, storage_capacity, scheduled_price, data_tables, discount_rate)
 
 # %% =====================================
 # Optimise MAVs
 importlib.reload(optimiser)
-RACK_TYPE = '5B_MAV'
-scenario_tables_optimum_MAV, revenue_MAV, kWh_export_MAV = optimise_layout(weather_simulation,
-                     RACK_TYPE, MODULE_TYPE, INSTALL_YEAR,
-                     DC_TOTAL, NUM_OF_ZONES, ZONE_AREA, RACK_INTERVAL_RATIO, TEMP_MODEL,
-                     EXPORT_LIM, STORAGE_CAPACITY, SCHEDULED_PRICE,
-                     DATA_TABLES, DISCOUNT_RATE)
+rack_type = '5B_MAV'
+scenario_tables_optimum_MAV, revenue_MAV, kWh_export_MAV = optimise_layout(weather_simulation,\
+     rack_type, module_type, install_year, DCTotal, num_of_zones, zone_area, rack_interval_ratio,\
+          temp_model, export_lim, storage_capacity, scheduled_price, data_tables, discount_rate)
 
 
 #%%
 # Call Monte Carlo Cost analysis
 
 # First generate data tables with the ScenarioID changed to something more intuitive
-new_data_tables = form_new_data_tables(DATA_TABLES,
-                                                [(scenario_tables_optimum_SAT, 'SAT'),
-                                                 (scenario_tables_optimum_MAV, 'MAV')])
+new_data_tables = form_new_data_tables(data_tables,[(scenario_tables_optimum_SAT, 'SAT'),\
+    (scenario_tables_optimum_MAV, 'MAV')])
 
 # Create iteration data
-data_tables_iter = create_iteration_tables(new_data_tables, 500, 0)
+data_tables_iter = create_iteration_tables(new_data_tables, 500, iteration_start=0)
 
 # Calculate cost result
-outputs_iter = calculate_scenarios_iterations(data_tables_iter, YEAR_ANALYSIS, 30)
-component_usage_y_iter, component_cost_y_iter, total_cost_y_iter, \
-     cash_flow_by_year_iter = outputs_iter
+outputs_iter = calculate_scenarios_iterations(data_tables_iter, year_start=analysis_year_start, analyse_years=30)
+component_usage_y_iter, component_cost_y_iter, total_cost_y_iter, cash_flow_by_year_iter = outputs_iter
 
 
 # %% ==========================================================
@@ -156,13 +134,11 @@ for (scenario_id, revenue_data, kWh_export_data) in [('MAV', revenue_MAV, kWh_ex
 
 # Now discount the costs, etc
 for col_name in ['cost', 'kWh', 'revenue']:
-    combined_scenario_data[col_name + '_disc'] = combined_scenario_data[col_name] / \
-         (1 + DISCOUNT_RATE) ** (combined_scenario_data['Year'] - YEAR_ANALYSIS)
+    combined_scenario_data[col_name + '_disc'] = combined_scenario_data[col_name] / (1 + discount_rate) ** (combined_scenario_data['Year'] - analysis_year_start)
 
 
 # Create a new table that removes the year, adding all the discounted flows
-discounted_sum = pd.pivot_table(combined_scenario_data, index=['Iteration', 'ScenarioID'], \
-    values=['kWh_disc', 'cost_disc', 'revenue_disc'])
+discounted_sum = pd.pivot_table(combined_scenario_data, index=['Iteration', 'ScenarioID'], values=['kWh_disc', 'cost_disc', 'revenue_disc'])
 
 # Now calculate LCOE and NPV
 
@@ -176,8 +152,8 @@ print(discounted_sum)
 for parameter in ['LCOE', 'NPV']:
     data = discounted_sum[parameter].reset_index()
     print(data)
-    data = pd.pivot_table(data, 'Iteration', parameter, 'ScenarioID')
-    data.plot.hist(50, 'step', 15)
+    data = pd.pivot_table(data, index='Iteration', values = parameter, columns='ScenarioID')
+    data.plot.hist(bins=50, histtype='step', fontsize=15)
     plt.title(parameter)
     plt.show()
 
@@ -185,45 +161,41 @@ for parameter in ['LCOE', 'NPV']:
 #%%
 
 # Calculate delta_LCOE and delta NPV for each iteration and plot this.
-SCENARIO_1 = 'MAV'
-SCENARIO_2 = 'SAT'
+
+scenario1 = 'MAV'
+scenario2 = 'SAT'
 
 for parameter in ['LCOE', 'NPV']:
     data = discounted_sum[parameter].reset_index()
-    data = pd.pivot_table(data, 'Iteration', parameter, 'ScenarioID')
+    data = pd.pivot_table(data, index='Iteration', values = parameter, columns='ScenarioID')
 
-    data['Difference'] = data[SCENARIO_2] - data[SCENARIO_1]
-    data['Difference'].plot.hist(50, 'step')
+    data['Difference'] = data[scenario2] - data[scenario1]
+    data['Difference'].plot.hist(bins=50, histtype='step')
     plt.title('Difference in '+ parameter)
     plt.show()
 
 # %%
 
-FONT_SIZE = 15
-rc = {'font.size': FONT_SIZE, 'axes.labelsize': FONT_SIZE, 'legend.fontsize': FONT_SIZE,
-      'axes.titlesize': FONT_SIZE, 'xtick.labelsize': FONT_SIZE, 'ytick.labelsize': FONT_SIZE}
+font_size = 15
+rc = {'font.size': font_size, 'axes.labelsize': font_size, 'legend.fontsize': font_size,
+      'axes.titlesize': font_size, 'xtick.labelsize': font_size, 'ytick.labelsize': font_size}
 plt.rcParams.update(**rc)
-plt.rc('font', 'bold')
+plt.rc('font', weight='bold')
 
 # For label titles
-fontdict = {'fontsize': FONT_SIZE, 'fontweight': 'bold'}
+fontdict = {'fontsize': font_size, 'fontweight': 'bold'}
 
-
-'''
-Function that generates a difference factor
-'''
-def generate_difference_factor(diff_factor, param_factor, scenario_1, scenario_2, parameter_name):
-    data_factor = diff_factor[param_factor].reset_index()
-    data_factor = pd.pivot_table(data_factor, 'Iteration', param_factor, 'ScenarioID')
-    data_factor[parameter_name] = data_factor[scenario_2] - data_factor[scenario_1]
+def generate_difference_factor(df, parameter, scenario_1, scenario_2, parameter_name):
+    data = df[parameter].reset_index()
+    data = pd.pivot_table(data, index = 'Iteration', values= parameter, columns = 'ScenarioID')
+    data[parameter_name] = data[scenario_2] - data[scenario_1]
     return data[parameter_name]
 
 
 # Sensitivity analysis / regression analysis to determine key uncertainties affecting these.
 parameters = generate_parameters(data_tables_iter)
 parameters_flat = parameters.copy()
-parameters_flat.columns = [group + ' ' + str(ID) + ' ' + var \
-    for (group, ID, var) in parameters_flat.columns.values]
+parameters_flat.columns = [group + ' ' + str(ID) + ' ' + var for (group, ID, var) in parameters_flat.columns.values]
 
 factor = generate_difference_factor(discounted_sum, 'LCOE', 'MAV', 'SAT', 'LCOE_Difference')
 parameters_flat = parameters_flat.join(factor)
@@ -235,19 +207,17 @@ calculate_variance_contributions(parameters_flat, 'LCOE_Difference')
 # %% ==========================================================
 # Present data from probabalistic analysis
 
-# cash_flow_transformed = pd.pivot_table(cash_flow_by_year_iter.reset_index(), \
-# columns='Iteration', index='Year')
+# cash_flow_transformed = pd.pivot_table(cash_flow_by_year_iter.reset_index(), columns='Iteration', index='Year')
 # revmax_direct = direct_revenue[racks_per_zone_max]
 # revmax_store = store_revenue[racks_per_zone_max]
 # revmax_total = total_revenue[racks_per_zone_max]
 # kWh_iter = kWh_discounted[racks_per_zone_max]
 # revmax_total_df = pd.DataFrame(revmax_total)
 # revenue_series_iter = sizing.align_cashflows(cash_flow_transformed, revmax_total_df)
-# npv_iter, yearly_npv_iter, npv_cost_iter, npv_revenue_iter, Yearly_NPV_revenue_iter, \
-# Yearly_NPV_costs_iter = sizing.get_npv(cash_flow_transformed, revenue_series_iter, \
-# discount_year)
+# npv_iter, yearly_npv_iter, npv_cost_iter, npv_revenue_iter, Yearly_NPV_revenue_iter, Yearly_NPV_costs_iter \
+#     = sizing.get_npv(cash_flow_transformed, revenue_series_iter, discount_rate)
 # LCOE_iter = npv_cost_iter/kWh_iter*100
 
-# FILENAME = RACK_TYPE + ' ' + MODULE_TYPE + ' install_' + str(INSTALL_YEAR)
-# npv_iter.to_csv('.\\Data\\OutputData\\' + FILENAME + ' NPV.csv')
-# LCOE_iter.to_csv('.\\Data\\OutputData\\' + FILENAME + ' LCOE.csv')
+# filename = rack_type + ' ' + module_type + ' install_' + str(install_year)
+# npv_iter.to_csv('.\\Data\\OutputData\\' + filename + ' NPV.csv')
+# LCOE_iter.to_csv('.\\Data\\OutputData\\' + filename + ' LCOE.csv')
