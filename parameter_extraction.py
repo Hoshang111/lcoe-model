750a12efb0acfa5e4e442617156e11fdef276c2d
phillhamer <p.hamer@unsw.edu.au>
2022-01-18 10:18:17 +1100

    added parameter extraction file

--- parameter_extraction.py
+++ parameter_extraction.py
@@ -7,13 +7,15 @@ Isc = 13.46
 Vmp = 43.10
 Imp = 12.65
 Rsh = Vmp / (0.2 * (Isc - Imp))
-Rs = 0.3
+Rs = 0.1
 
 def func(x):
     return [x[0] - x[1] * (math.exp(x[2] * Isc * Rs) - 1) - Isc * Rs / Rsh - Isc,
             x[0] - x[1] * (math.exp(x[2] * Voc) - 1) - Voc / Rsh,
-            x[0] - x[1] * (math.exp(x[2] * (Vmp + Imp * Rs)) - 1) - (Voc + Isc * Rs) / Rsh - Imp ]
+            x[0] - x[1] * (math.exp(x[2] * (Vmp + Imp * Rs)) - 1) - (Voc + Imp * Rs) / Rsh - Imp ]
 
-root = fsolve(func, [13.6, 1e-10, 2])
+root = fsolve(func, [13.6, 1e-10, 1.5])
+
 root
-np.isclose(func(root),[0.0, 0.0, 0.0])
+
+np.isclose(func(root), [0.0, 0.0, 0.0])
