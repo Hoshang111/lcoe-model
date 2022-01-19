f06a6ae61807125c2a96ac19d75d86ccdac4f4c2
phillhamer <p.hamer@unsw.edu.au>
2022-01-19 23:33:36 +1100

    generation of numbers for Longi modules (V1)

--- Single_diode_generation.py
+++ Single_diode_generation.py
@@ -1,4 +1,4 @@
 import pvlib
 
-results = pvlib.pvsystem.singlediode(photocurrent=13.464, saturation_current=6e-11, resistance_series=0.4,
-                                     resistance_shunt=500, nNsVth=2)
+results = pvlib.pvsystem.singlediode(photocurrent=13.469, saturation_current=6e-11, resistance_series=0.23,
+                                     resistance_shunt=250, nNsVth=1.9)
