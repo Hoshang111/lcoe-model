fcaa8a0a51db7f2e1541f5b4ed77a1dec49cf602
phillhamer <p.hamer@unsw.edu.au>
2022-02-16 12:14:56 +1100

    playing around with how we generate dni

--- Phill_Dni.py
+++ Phill_Dni.py
@@ -5,6 +5,7 @@ import pvlib
 import pytz
 import os
 import simulation_functions as func
+import math
 # PV-Lib imports
 from pvlib import pvsystem
 from pvlib.pvsystem import PVSystem, FixedMount
@@ -24,6 +25,19 @@ solpos = location.get_solarposition(weather_simulation.index)
 clearsky = location.get_clearsky(weather_simulation.index)
 suntimes = location.get_sun_rise_set_transit(weather_simulation.index, method='spa')
 transit_zenith = location.get_solarposition(suntimes['transit'])
+
+## Create Dni lookup table based on 5 minute data
+dt_lookup = pd.date_range(start= weather_simulation.index[0],
+                          end= weather_simulation.index[-1], freq='5T')
+clearsky_lookup = location.get_clearsky(dt_lookup)
+solpos_lookup = location.get_solarposition(dt_lookup)
+zenith_to_rad = np.radians(solpos_lookup)
+cos_lookup = np.cos(zenith_to_rad)
+horizontal_dni_lookup = cos_lookup['apparent_zenith']*clearsky_lookup['dni']
+horizontal_dni_lookup[horizontal_dni_lookup<0] = 0
+hourly_lookup = horizontal_dni_lookup.resample('H').mean()
+
+
 
 
 
