91aac8ca9195800c8a69d5eb399394d7e3739d1d
phill <p.hamer@unsw.edu.au>
2022-05-17 21:03:39 +1000

    playing with weather files and sorting

--- Functions/weather_analysis.py
+++ Functions/weather_analysis.py
@@ -254,7 +254,7 @@ def generate_TMY(weather_dict):
         month_data = weather_percentile(0.5, weather_dict, month)
         TMY = pd.concat([TMY, month_data], axis=0)
 
-    measure_freq = pd.infer_freq(weather_dict['jan'][0])
+    measure_freq = pd.infer_freq(weather_dict['jan'][0].index)
     TMY_index = pd.date_range(start='1/1/1990', end='12/31/1990', freq=measure_freq, tz=pytz.UTC)
     TMY = TMY.set_index(TMY_index)
 
