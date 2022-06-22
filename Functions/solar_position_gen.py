9afaeb5129d9bd60ffe19ba802b99ef30f5f0c53
phill <p.hamer@unsw.edu.au>
2022-06-22 21:39:09 +1000

    trying to generate mc weather file

--- Functions/solar_position_gen.py
+++ Functions/solar_position_gen.py
@@ -8,7 +8,7 @@ coordinates = [(27.671, 85.4298, 'Bhaktapur', 1401, 'Asia/Kathmandu')]  # Coordi
 latitude, longitude, name, altitude, timezone = coordinates[0]
 location = Location(latitude, longitude, name=name, altitude=altitude, tz=timezone)
 
-simulation_period = pd.date_range(start='1/1/2018 00:00:00', end='7/1/2018 23:59:00', freq='H')
+simulation_period = pd.date_range(start='1/1/2018 00:00:00', end='7/1/2018 23:59:00', freq='H', timezone=timezone)
 
 solar_position = location.get_solarposition(simulation_period)
 clearsky = location.get_clearsky(simulation_period)
