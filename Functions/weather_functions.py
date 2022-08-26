fe84f16e1776f0a06302ef43d9c45c5ae3b80a25
phill <p.hamer@unsw.edu.au>
2022-08-26 15:54:42 -0500

    trying to generate mc weather file

--- Functions/weather_functions.py
+++ Functions/weather_functions.py
@@ -14,14 +14,13 @@ def weather_nofit(ground, satellite, fig_name):
     """"""
     x = satellite
     y = ground
-    z = x
     fig, ax = plt.subplots(figsize=(25, 20))
     ax.scatter(x, y)
     ax.set_xlabel('Satellite', **fontdict)
     ax.set_ylabel('Ground', **fontdict)
     ax.set_title(fig_name)
 
-    ax.plot(x, z, linewidth=3, colour='C1')
+    ax.plot(x, x * 1, linewidth=3, colour='C1')
     # plt.show()
     save_path = "C:\\Users\phill\Documents\Bangladesh Application\weather_data/" + fig_name
     plt.savefig(save_path, dpi=300, bbox_inches='tight')
