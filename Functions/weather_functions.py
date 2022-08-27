42d3298fed3cca7568b61d032435185119099fe4
phill <p.hamer@unsw.edu.au>
2022-08-26 20:49:53 -0500

    trying to generate mc weather file

--- Functions/weather_functions.py
+++ Functions/weather_functions.py
@@ -41,6 +41,7 @@ def weather_overlay(initial_data, masked_data, calc_data, fig_name):
     ax.set_xlabel('Calculated', **fontdict)
     ax.set_ylabel('Measured', **fontdict)
     ax.set_title(fig_name)
+    ax.axline((0, 0), slope=1, label='m=1')
 
     save_path = "C:\\Users\phill\Documents\Bangladesh Application\weather_data/" + fig_name
     plt.savefig(save_path, dpi=300, bbox_inches='tight')
