7f458ac639aee7e3237d30b767202d5a6bdec5b2
phill <p.hamer@unsw.edu.au>
2022-09-27 15:05:24 +1000

    playing with applying corrections to news files

--- Functions/apply_corrections.py
+++ Functions/apply_corrections.py
@@ -10,23 +10,13 @@ file_path = os.path.join(data_path, "corrections.p")
 corrections = cpickle.load(open(file_path, 'rb'))
 
 #%% ====================================================
-# import ground and satellite data
+# import satellite data
 data_path = "C:\\Users\phill\Documents\Bangladesh Application\weather_data"
-ground_path = os.path.join(data_path, "qcrad_data.csv")
-ground_data = pd.read_csv(ground_path, index_col=0, header=0)
-ground_data.set_index(pd.to_datetime(ground_data.index), inplace=True)
+satellite_path = os.path.join(data_path, "qcrad_data.csv")
+satellite_data_init = pd.read_csv(satellite_path, index_col=0, header=0)
+satellite_data_init.set_index(pd.to_datetime(satellite_data_init.index), inplace=True)
 
-satellite_data_2017_path = os.path.join(data_path, "Feni_Himawari_2017.csv")
-satellite_data_2018_path = os.path.join(data_path, "Feni_Himawari_2018.csv")
-satellite_data_2019_path = os.path.join(data_path, "Feni_Himawari_2019.csv")
-satellite_data_2017 = pd.read_csv(satellite_data_2017_path, header=2)
-satellite_data_2018 = pd.read_csv(satellite_data_2018_path, header=2)
-satellite_data_2019 = pd.read_csv(satellite_data_2019_path, header=2)
-satellite_data_2017.set_index(pd.to_datetime(satellite_data_2017[["Year", "Month", "Day", "Hour", "Minute"]]), inplace=True)
-satellite_data_2018.set_index(pd.to_datetime(satellite_data_2018[["Year", "Month", "Day", "Hour", "Minute"]]), inplace=True)
-satellite_data_2019.set_index(pd.to_datetime(satellite_data_2019[["Year", "Month", "Day", "Hour", "Minute"]]), inplace=True)
-satellite_data_localtz = pd.concat([satellite_data_2017, satellite_data_2018, satellite_data_2019], axis=0)
-satellite_data_aware = satellite_data_localtz.tz_localize("Asia/Dhaka")
+satellite_data_aware = satellite_data_init.tz_localize("Asia/Dhaka")
 satellite_data = satellite_data_aware.tz_convert("UTC")
 satellite_data = satellite_data.rename(columns={'GHI':'ghi',
                                                 'DHI':'dhi',
@@ -43,56 +33,31 @@ satellite_data_aware = satellite_data_aware.rename(columns={'GHI':'ghi',
                                                 'Clearsky GHI':'clearsky ghi',
                                                 'Cloud Type':'cloud type'})
 
-#%% Align Data
-# satellite_data_aligned =
-ground_data_mod = ground_data.shift(periods=5, freq='T')
-ground_data_hourly = ground_data_mod.resample('10T', axis=0).mean()
-# ground_data_hourly = ground_data_hourlyA.shift(periods=-5, freq='T')
-satellite_data_aligned = satellite_data.reindex(ground_data_hourly.index)
-
-ground_dhi = ground_data_hourly['dhi']
-ground_ghi = ground_data_hourly['ghi']
-ground_dni = satellite_data_aligned['dni']
-
-satellite_dhi = satellite_data_aligned['dhi']
-satellite_ghi = satellite_data_aligned['ghi']
-satellite_dni = satellite_data_aligned['dni']
 
 # %% ===========================================================
 # Define conditions of interest: Cloud types
-satellite_cloud0 = satellite_data_aligned.loc[satellite_data['cloud type'] == 0, ['ghi', 'dhi', 'dni']]
-ground_cloud0 = ground_data_hourly.loc[satellite_data['cloud type'] == 0, ['ghi', 'dhi', 'dni']]
+satellite_cloud0 = satellite_data.loc[satellite_data['cloud type'] == 0, ['ghi', 'dhi', 'dni']]
 
-satellite_cloud1 = satellite_data_aligned.loc[satellite_data['cloud type'] == 1, ['ghi', 'dhi', 'dni']]
-ground_cloud1 = ground_data_hourly.loc[satellite_data['cloud type'] == 1, ['ghi', 'dhi', 'dni']]
+satellite_cloud1 = satellite_data.loc[satellite_data['cloud type'] == 1, ['ghi', 'dhi', 'dni']]
 
-satellite_cloud2 = satellite_data_aligned.loc[satellite_data['cloud type'] == 2, ['ghi', 'dhi', 'dni']]
-ground_cloud2 = ground_data_hourly.loc[satellite_data['cloud type'] == 2, ['ghi', 'dhi', 'dni']]
+satellite_cloud2 = satellite_data.loc[satellite_data['cloud type'] == 2, ['ghi', 'dhi', 'dni']]
 
-satellite_cloud3 = satellite_data_aligned.loc[satellite_data['cloud type'] == 3, ['ghi', 'dhi', 'dni']]
-ground_cloud3 = ground_data_hourly.loc[satellite_data['cloud type'] == 3, ['ghi', 'dhi', 'dni']]
+satellite_cloud3 = satellite_data.loc[satellite_data['cloud type'] == 3, ['ghi', 'dhi', 'dni']]
 
-satellite_cloud4 = satellite_data_aligned.loc[satellite_data['cloud type'] == 4, ['ghi', 'dhi', 'dni']]
-ground_cloud4 = ground_data_hourly.loc[satellite_data['cloud type'] == 4, ['ghi', 'dhi', 'dni']]
+satellite_cloud4 = satellite_data.loc[satellite_data['cloud type'] == 4, ['ghi', 'dhi', 'dni']]
 
-# satellite_cloud5 = satellite_data_aligned.loc[satellite_data['cloud type'] == 5, ['ghi']]
-# ground_cloud5 = ground_data_hourly.loc[satellite_data['cloud type'] == 5, ['ghi']]
+# satellite_cloud5 = satellite_data.loc[satellite_data['cloud type'] == 5, ['ghi', 'dhi'], 'dni']
 
-satellite_cloud6 = satellite_data_aligned.loc[satellite_data['cloud type'] == 6, ['ghi', 'dhi', 'dni']]
-ground_cloud6 = ground_data_hourly.loc[satellite_data['cloud type'] == 6, ['ghi', 'dhi', 'dni']]
+satellite_cloud6 = satellite_data.loc[satellite_data['cloud type'] == 6, ['ghi', 'dhi', 'dni']]
 
-satellite_cloud7 = satellite_data_aligned.loc[satellite_data['cloud type'] == 7, ['ghi', 'dhi', 'dni']]
-ground_cloud7 = ground_data_hourly.loc[satellite_data['cloud type'] == 7, ['ghi', 'dhi', 'dni']]
+satellite_cloud7 = satellite_data.loc[satellite_data['cloud type'] == 7, ['ghi', 'dhi', 'dni']]
 
 cloud_list_satellite = [satellite_cloud0, satellite_cloud1, satellite_cloud2, satellite_cloud3, satellite_cloud4,
               satellite_cloud6, satellite_cloud7]
 
-cloud_list_ground = [ground_cloud0, ground_cloud1, ground_cloud2, ground_cloud3, ground_cloud4,
-              ground_cloud6, ground_cloud7]
-
 labels = ['cloud0', 'cloud1', 'cloud2', 'cloud3', 'cloud4', 'cloud6', 'cloud7']
 
-cloud_zip = list(zip(cloud_list_satellite, cloud_list_ground, labels))
+cloud_zip = list(zip(cloud_list_satellite, labels))
 
 # %% =======================================================
 dhi_corrected = []
